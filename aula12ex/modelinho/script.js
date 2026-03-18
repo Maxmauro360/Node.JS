0e49cc9a76ed6f888a6545e565414bc6c36f3eb9
Max Mauro <maxmaurocunha22@gmail.com>
2026-03-18 18:54:32 -0300

    quase
    usar o <body onload = "carregar()" > faz a função rodar assim que a pagina abre, o certo é colocar depois que o usuario digitar <button onclick="carregar()">Verificar</button>

--- aula12ex/modelinho/script.js
+++ aula12ex/modelinho/script.js
@@ -1,31 +1,32 @@
 
 function carregar(){
-
-    const msg = window.document.querySelector('#texto1')
-    const msg2 = window.document.querySelector('#texto2')
-    const msg3 = window.document.querySelector('#texto3')
+    const txt1 = window.document.querySelector('#texto1')
+    const txt2 = window.document.querySelector('#texto2')
+    const txt3 = window.document.querySelector('#texto3')
     const idadeinput = window.document.querySelector('#idadeinput')
-    
     const idade = Number(idadeinput.value)
-    msg.textContent ='Selecione sua idade:'
-    msg2.innerHTML= `Você é ${tipo} e tem ${idade} anos `
-    msg3.textContent = 'Preencha os dados acima para ver o resultado!'
+    let tipo =''
+    txt1.textContent='Selecione sua idade:'+ idade
+    txt2.textContent= 'Eai'
+    
+    
     
     if(idade<=4 ){
         tipo= 'nenem'
-         
+        
     } else if (idade <=12 ){
         tipo = 'criança'
                
     } else if (idade<=17){
         tipo = 'adolescente'
-      
+        
     }else if (idade<=59){
         tipo ='adulto'
         
     }else{
-         tipo ='idoso'
+        tipo ='idoso'
         
     }
+    txt2nnerHTML= `Você é ${tipo} e tem ${idade} anos `
 }
     
