4504435b57ba8f7ce428b660fbc95964c3bed189
Max Mauro <maxmaurocunha22@gmail.com>
2026-03-19 17:39:14 -0300

    Finalmente
    para jogar criar uma função no Js:
    document.addEventListener('DOMContentLoaded', function(){
        corpo()
    })
    
    o uso de uma ' ou " faz toda diferença, isso implica do programa rodar ou não

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,31 +1,15 @@
+function corpo(){
+   const h = window.document.querySelector('#header')
+   const txt1 = window.document.querySelector("#texto1")
+   const txt2 = window.document.querySelector("#texto2")
+   const colocar = window.document.querySelector("#anexar")
+   colocar.textContent = 'aqui'
 
-function carregar(){
-    const txt2 = window.document.querySelector('#texto2')
-    const txt3 = window.document.querySelector('#texto3')
-    const idadeinput = window.document.querySelector('#idadeinput')
-    const idade = Number(idadeinput.value)
-    let tipo =''
-    
-    
-    
-    txt3.innerHTML= '<strong>Funcionou</strong>'
-    
-    if(idade<=4 ){
-        tipo= 'nenem'
-        
-    } else if (idade <=12 ){
-        tipo = 'criança'
-        
-    } else if (idade<=17){
-        tipo = 'adolescente'
-        
-    }else if (idade<=59){
-        tipo ='adulto'
-        
-    }else{
-        tipo ='idoso'
-        
-    }
-    txt2.innerHTML=`Você é ${tipo} e tem ${idade} anos `
+   txt1.textContent ='Está rodando'
+   txt2.textContent ='Tambem esta rodando'
+   h.textContent= 'foi'
+   
 }
-    
+document.addEventListener('DOMContentLoaded', function(){
+    corpo()
+})
