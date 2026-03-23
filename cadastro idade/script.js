d45b10dfe2699c0c519a80668eb0df51e0f8450c
Max Mauro <maxmaurocunha22@gmail.com>
2026-03-23 01:55:45 -0300

    LABEL
    a label no css faz toda a diferença quando quiser colocar um texto com input alinhado

--- cadastro idade/script.js
+++ cadastro idade/script.js
@@ -1,10 +1,14 @@
 function corpo(){    
     const header = window.document.querySelector("#cabeça");
+    const txt = window.document.querySelector("#texto");
     const txt1 = window.document.querySelector("#texto1");
     const txt2 = window.document.querySelector("#texto2");
     const txt3 = window.document.querySelector("#texto3");
+    const inp = window.document.querySelector("#textoano");
     
+    txt.textContent ='Selecione seu nascimento:'
     header.textContent = 'texto';
+
 
     txt1.textContent ='olá';
     
