f2b4dd0449581a94fb87a6c27dac69dec54d4d24
Max Mauro <maxmaurocunha22@gmail.com>
2026-03-23 03:55:31 -0300

    Resolvido
    reestudar as linhas de codigo

--- cadastro idade/script.js
+++ cadastro idade/script.js
@@ -1,15 +1,33 @@
 function corpo(){    
+    
     const header = window.document.querySelector("#cabeça");
     const txt = window.document.querySelector("#texto");
     const txt1 = window.document.querySelector("#texto1");
-    const txt2 = window.document.querySelector("#texto2");
-    const txt3 = window.document.querySelector("#texto3");
     const inp = window.document.querySelector("#textoano");
     
-    txt.textContent ='Selecione seu nascimento:'
-    header.textContent = 'texto';
+    txt.textContent ='Selecione seu nascimento:';
+    txt1.textContent = 'Alienígena';
+    header.textContent = 'Verificador de idade';
+}
+ function verificar(){   
+     let data = new Date();
+     let ano = data.getFullYear();
+     let fano = window.document.querySelector("#textoano");
+     let res = window.document.querySelector("#resultado");
+     if(fano.value.length == 0 || Number(fano.value)> ano){
+        alert("Verifdique os dados!")
 
-
-    txt1.textContent ='olá';
-    
+     }else{
+         let fsex = window.document.getElementsByName("radsex")
+         let idade = ano - Number(fano.value);
+         let genero = '';
+         if (fsex[0].checked){
+            genero= "Homem";
+        }else if (fsex[1].checked){
+            genero = "Mulher";
+        } else{
+            genero = "Alienígena";
+        }
+        res.innerHTML = `Você é um(a)${genero} e tem ${idade} anos`
+    }
 }
